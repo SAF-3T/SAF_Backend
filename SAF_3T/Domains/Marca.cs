e067cb1a8d2fb980072aeedd09460a6bc79c45ae
Vitor Luiz <Vitor.sesi21@gmail.com>
2022-04-13 15:58:16 -0300

    APIinicial

---
+++ SAF_3T/Domains/Marca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace SAF_3T.Domains
+{
+    public partial class Marca
+    {
+        public Marca()
+        {
+            Veiculos = new HashSet<Veiculo>();
+        }
+
+        public byte IdMarca { get; set; }
+
+        [Required(ErrorMessage = "É preciso informar qual a marca")]
+        public string NomeMarca { get; set; }
+
+        public virtual ICollection<Veiculo> Veiculos { get; set; }
+    }
+}
